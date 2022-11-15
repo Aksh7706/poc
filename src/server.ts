427b4a3c1fc938a56d684167ed131e09feb682eb
Aksh7706 <46279921+Aksh7706@users.noreply.github.com>
2022-11-15 22:34:40 +0530

    Cors

--- src/server.ts
+++ src/server.ts
@@ -31,6 +31,15 @@ function setUpSecurityHeaders(app: Express): void {
     res.setHeader('X-Frame-Options', 'SAMEORIGIN');
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('Content-Security-Policy', "default-src 'none'");
+    res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
     next();
   });
 }
