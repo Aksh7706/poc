9defc72146fdb1de3881ef6fb45cd1f8b2382a33
Akshay Prakash <akshay.prakash@walmart.com>
2022-11-20 19:07:06 +0530

    FIX Temp

--- dist/src/utils/base64.js
+++ dist/src/utils/base64.js
@@ -9,15 +9,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Base64 = void 0;
 class Base64 {
     static encode(data) {
-        var encodedStr = btoa(data);
-        var removeSpecial = encodedStr.replaceAll('+', '_');
-        var urlSafe = removeSpecial.replaceAll('/', '-');
-        return urlSafe;
+        return data;
+        // if(!data.includes('.')) return data;
+        // var encodedStr = btoa(data);
+        // var removeSpecial = encodedStr.replaceAll('+', '_');
+        // var urlSafe = removeSpecial.replaceAll('/', '-');
+        // return urlSafe;
     }
     static decode(data) {
-        var urlFix = data.replaceAll('-', '/');
-        var encodedStr = urlFix.replaceAll('_', '+');
-        return atob(encodedStr);
+        return data;
+        // try{
+        //     var urlFix = data.replaceAll('-', '/');
+        //     var encodedStr = urlFix.replaceAll('_', '+');
+        //     return atob(encodedStr);
+        // } catch (e) {
+        //     console.log("Decode Error : ", e)
+        //     return data;
+        // }
     }
 }
 exports.Base64 = Base64;
--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -7,15 +7,23 @@
 
 export class Base64 {
   static encode(data: string) {
-    var encodedStr = btoa(data);
-    var removeSpecial = encodedStr.replaceAll('+', '_');
-    var urlSafe = removeSpecial.replaceAll('/', '-');
-    return urlSafe;
+    return data;
+    // if(!data.includes('.')) return data;
+    // var encodedStr = btoa(data);
+    // var removeSpecial = encodedStr.replaceAll('+', '_');
+    // var urlSafe = removeSpecial.replaceAll('/', '-');
+    // return urlSafe;
   }
 
   static decode(data: string) {
-    var urlFix = data.replaceAll('-', '/');
-    var encodedStr = urlFix.replaceAll('_', '+');
-    return atob(encodedStr);
+    return data;
+    // try{
+    //     var urlFix = data.replaceAll('-', '/');
+    //     var encodedStr = urlFix.replaceAll('_', '+');
+    //     return atob(encodedStr);
+    // } catch (e) {
+    //     console.log("Decode Error : ", e)
+    //     return data;
+    // }
   }
 }
